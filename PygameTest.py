28deea4165495eb84593e48231b853c21f53eb6e
veroni <veronidenise@gmail.com>
2022-11-27 22:38:53 +0100

    add an obstacle

--- PygameTest.py
+++ PygameTest.py
@@ -5,17 +5,24 @@ R_BORDER = 300
 SCREEN = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption("Basic Game")
 
-LIGHTBLUE = (150, 200, 255)
+VEL = 10
 BLACK = (0, 0, 0)
+GREY = (60, 60, 60)
 WHITE = (255, 255, 255)
 FPS = 60
 FIGURE = p.image.load('stick_figure.png')
+OBJECT= p.image.load('object1.png')
 main_character = p.transform.scale(FIGURE, (600, 600))
+main_object = p.transform.scale(OBJECT, (40, 25))
 
 
 def draw_window(m_c):
     SCREEN.fill(WHITE)
     SCREEN.blit(main_character, (m_c.x, m_c.y))
+    SCREEN.blit(main_object, (450, 222))
+    p.draw.line(SCREEN, GREY, (0, 245), (600, 245), 1)
+    #p.draw.line(SCREEN, GREY, (0, 55), (600, 55), 1)
+    #p.draw.rect(SCREEN, GREY, p.Rect(450, 220, 25, 25), 1)
     p.display.update()
 
 def move_main_character(e, m_c):
@@ -24,6 +31,9 @@ def move_main_character(e, m_c):
             m_c.x += 10
         if e.key == p.K_LEFT and m_c.x >= -275:
             m_c.x -= 10
+
+#def move_main_object():
+
 
 
 def main():
@@ -37,6 +47,7 @@ def main():
             if e.type == p.QUIT:
                 run = False
             move_main_character(e, m_c)
+            #move_obstacle()
         draw_window(m_c)
 
     p.quit()

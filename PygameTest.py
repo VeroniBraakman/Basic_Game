4a697d225f4561317956a7a89cc5b11c8024915b
veroni <veronidenise@gmail.com>
2022-11-29 21:43:42 +0100

    move an obstacle

--- PygameTest.py
+++ PygameTest.py
@@ -5,7 +5,7 @@ R_BORDER = 300
 SCREEN = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption("Basic Game")
 
-VEL = 10
+VEL = 1
 BLACK = (0, 0, 0)
 GREY = (60, 60, 60)
 WHITE = (255, 255, 255)
@@ -16,12 +16,11 @@ main_character = p.transform.scale(FIGURE, (600, 600))
 main_object = p.transform.scale(OBJECT, (40, 25))
 
 
-def draw_window(m_c):
+def draw_window(m_c, m_o):
     SCREEN.fill(WHITE)
     SCREEN.blit(main_character, (m_c.x, m_c.y))
-    SCREEN.blit(main_object, (450, 222))
+    SCREEN.blit(main_object, (m_o.x -10, m_o.y))
     p.draw.line(SCREEN, GREY, (0, 245), (600, 245), 1)
-    #p.draw.line(SCREEN, GREY, (0, 55), (600, 55), 1)
     #p.draw.rect(SCREEN, GREY, p.Rect(450, 220, 25, 25), 1)
     p.display.update()
 
@@ -32,23 +31,26 @@ def move_main_character(e, m_c):
         if e.key == p.K_LEFT and m_c.x >= -275:
             m_c.x -= 10
 
-#def move_main_object():
-
-
+def move_main_object(m_o):
+    p.time.delay(10)
+    m_o.x = m_o.x - VEL
+    if m_o.x < 0:
+        m_o.x = 450
 
 def main():
     m_c = p.Rect((-250, 25), (10, 10))
+    m_o = p.Rect((450, 222), (10, 10))
 
     clock = p.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
+        move_main_object(m_o)
         for e in p.event.get():
             if e.type == p.QUIT:
                 run = False
             move_main_character(e, m_c)
-            #move_obstacle()
-        draw_window(m_c)
+        draw_window(m_c, m_o)
 
     p.quit()
 

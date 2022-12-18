25db3e34a010223bdcd842c663b0e6172bb93eae
veroni <veronidenise@gmail.com>
2022-12-18 17:13:59 +0100

    continuous movement

--- PygameTest.py
+++ PygameTest.py
@@ -16,6 +16,7 @@ FIGURE = p.image.load('poppetje.png')
 OBJECT= p.image.load('object1.png')
 main_character = p.transform.scale(FIGURE, (50,80))
 main_object = p.transform.scale(OBJECT, (40, 25))
+jump = False
 
 
 def draw_window(m_c, m_o):
@@ -25,12 +26,12 @@ def draw_window(m_c, m_o):
     p.draw.line(SCREEN, GREY, (0, 245), (600, 245), 1)
     p.display.update()
 
-def move_main_character(e, m_c):
-    if e.type == p.KEYDOWN:
-        if e.key == p.K_RIGHT and m_c.x <= R_BORDER:
-            m_c.x += 10
-        if e.key == p.K_LEFT and m_c.x >= 0:
-            m_c.x -= 10
+def move_main_character(m_c):
+    key_input = p.key.get_pressed()
+    if key_input[p.K_RIGHT] and m_c.x <= R_BORDER:
+        m_c.x += VEL
+    if key_input[p.K_LEFT] and m_c.x >= 0:
+        m_c.x -= VEL
 
 def detect_collision(m_o, m_c):
     if m_o.x < m_c.x:
@@ -54,8 +55,11 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 run = False
-            move_main_character(e, m_c)
-            detect_collision(m_o, m_c)
+
+
+        #m_c_jump(e, m_c)
+        move_main_character(m_c)
+        detect_collision(m_o, m_c)
         draw_window(m_c, m_o)
 
     p.quit()

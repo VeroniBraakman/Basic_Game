aa2fd84f7c4182522596a66556312b57017dd1c4
veroni <veronidenise@gmail.com>
2023-01-01 21:10:12 +0100

    jumping

--- PygameTest.py
+++ PygameTest.py
@@ -7,7 +7,7 @@ R_BORDER = 570
 SCREEN = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption("Basic Game")
 
-VEL = 1
+VEL = 2
 BLACK = (0, 0, 0)
 GREY = (60, 60, 60)
 WHITE = (255, 255, 255)
@@ -17,7 +17,6 @@ OBJECT= p.image.load('object1.png')
 main_character = p.transform.scale(FIGURE, (50,80))
 main_object = p.transform.scale(OBJECT, (40, 25))
 jump = False
-
 
 def draw_window(m_c, m_o):
     SCREEN.fill(WHITE)
@@ -29,46 +28,38 @@ def draw_window(m_c, m_o):
 def move_main_character(m_c):
     key_input = p.key.get_pressed()
     if key_input[p.K_RIGHT] and m_c.x <= R_BORDER:
-        m_c.x += VEL
+        m_c.x += 2
     if key_input[p.K_LEFT] and m_c.x >= 0:
-        m_c.x -= VEL
-
-def m_c_jump(m_c):
-    y_gravity = 1
-    jump_height = 2
-    y_velocity = jump_height
-
-
-    key_input = p.key.get_pressed()
-    if key_input[p.K_SPACE]:
-        m_c.y -= y_velocity
-        y_velocity -= y_gravity
-        if y_velocity < jump_height:
-            y_velocity = jump_height
-
-
-
+        m_c.x -= 2
 
 def detect_collision(m_o, m_c):
-    if m_o.x < m_c.x and m_c.y >= 149:
+    #if m_o.x < m_c.x and m_c.y >= 168:
+    #    m_c.x = 0
+    if m_c.x == m_o.x and m_c.y > 158:
+        print("Hello")
         m_c.x = 0
+        # dit werkt niet goed.
 
 def move_main_object(m_o):
     p.time.delay(10)
     m_o.x = m_o.x - VEL
+    #print(m_o.x)
     if m_o.x < -40:
         m_o.x = 620
+
 
 def main():
     m_c = p.Rect((0, 168), (10, 10))
     m_o = p.Rect((620, 222), (10, 10))
+    jump = False
+    jumpCount = 0
+    jumpMax = 15
 
     clock = p.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
         move_main_object(m_o)
-        m_c_jump(m_c)
         move_main_character(m_c)
         detect_collision(m_o, m_c)
         draw_window(m_c, m_o)
@@ -76,6 +67,17 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 run = False
+            if e.type == p.KEYDOWN:
+                if not jump and e.key == p.K_SPACE:
+                    jump = True
+                    jumpCount = jumpMax
+
+        if jump:
+            m_c.y -= jumpCount
+            if jumpCount > -jumpMax:
+                jumpCount -= 1
+            else:
+                jump = False
 
     p.quit()
 

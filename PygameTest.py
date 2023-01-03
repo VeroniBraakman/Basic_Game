3c1b9a1e3b8afdf81163622b86f845973898503f
veroni <veronidenise@gmail.com>
2023-01-03 19:48:19 +0100

    text

--- PygameTest.py
+++ PygameTest.py
@@ -4,7 +4,7 @@ p.init()
 
 WIDTH, HEIGHT = 600, 300
 SCREEN = p.display.set_mode((WIDTH, HEIGHT))
-p.display.set_caption("Basic Game")
+p.display.set_caption("Game")
 
 VEL = 2
 BLACK = (0, 0, 0)
@@ -14,22 +14,49 @@ FPS = 60
 jump = False
 
 def draw_window():
-    x = 320
-    y = 225
     SCREEN.fill(WHITE)
+    #ground
     p.draw.line(SCREEN, GREY, (0, 245), (600, 245), 1)
-    obstacle = p.draw.rect(SCREEN, BLACK, p.Rect(x, y, 20, 20))
+    #flag
+    p.draw.line(SCREEN, GREY, (550, 245), (550, 180), 3)
+    p.draw.line(SCREEN, GREY, (550, 180), (570, 200), 3)
+    p.draw.line(SCREEN, GREY, (550, 200), (570, 200), 3)
+
+
+def create_obstacle(obstacle):
+    p.draw.rect(SCREEN, BLACK, p.Rect(obstacle.x, obstacle.y, 20, 20))
+
+def move_obstacle(obstacle, hero):
+    if hero.x <= 550:
+        p.time.delay(10)
+        obstacle.x -= VEL
+        if obstacle.x <= 0:
+            obstacle.x = WIDTH
 
 def create_a_hero(hero):
-    p.draw.rect(SCREEN, BLACK, p.Rect(hero.x, hero.y, 10, 80), 2)
+    p.draw.rect(SCREEN, BLACK, p.Rect(hero.x, hero.y, 10, 50), 2)
     key_input = p.key.get_pressed()
     if key_input[p.K_RIGHT] and hero.x <= WIDTH:
         hero.x += 2
     if key_input[p.K_LEFT] and hero.x >= 0:
         hero.x -= 2
 
+def check_collision(hero, obstacle):
+    collide = p.Rect.colliderect(hero, obstacle)
+    if collide:
+        hero.x = 20
+
+def winning(hero):
+    if  hero.x >= 550:
+        font = p.font.Font('freesansbold.ttf', 26)
+        text = font.render('You Won!', True, WHITE, BLACK)
+        textRect = text.get_rect()
+        textRect.center = (500, 50)
+        SCREEN.blit(text, textRect)
+
 def main():
-    hero = p.Rect((20, 165), (10, 80))
+    hero = p.Rect((20, 195), (10, 50))
+    obstacle = p.Rect((WIDTH, 225, 20, 20))
     jump = False
     jumpCount = 0
     jumpMax = 15
@@ -40,6 +67,10 @@ def main():
         clock.tick(FPS)
         draw_window()
         create_a_hero(hero)
+        create_obstacle(obstacle)
+        move_obstacle(obstacle, hero)
+        check_collision(hero, obstacle)
+        winning(hero)
         p.display.update()
 
         for e in p.event.get():
